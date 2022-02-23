5a269b95cfc13316dddb3428b66e5af27bb5a8f4
juanMoleroD <juanldimeco@gmail.com>
2022-02-23 22:25:04 +0000

    Passed first test with one word and an upper case

--- src/main/java/kyu7/reverseword/ReverseWordsKata.java
+++ src/main/java/kyu7/reverseword/ReverseWordsKata.java
@@ -1,9 +1,25 @@
 package kyu7.reverseword;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class ReverseWordsKata {
 
     public String reverseWords(final String original) {
+        char[] words = original.toCharArray();
+        StringBuilder result = new StringBuilder();
 
-        return "";
+        for (int i = (words.length -1), j = 0; i >= 0 ; i--,j++){
+            result.append(words[i]);
+        }
+
+        return result.toString();
     }
+
+    public static void main(String[] args) {
+        ReverseWordsKata kata = new ReverseWordsKata();
+        System.out.println(kata.reverseWords("Words"));
+    }
+
+
 }
