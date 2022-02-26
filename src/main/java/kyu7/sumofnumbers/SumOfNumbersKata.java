66b1325836fb5f97e4df6d1535fc0ee1f0427c84
juanMoleroD <juanldimeco@gmail.com>
2022-02-26 22:19:37 +0000

    replaced if else statement for math min/max, and rearranged for readability. Much more elegant.

--- src/main/java/kyu7/sumofnumbers/SumOfNumbersKata.java
+++ src/main/java/kyu7/sumofnumbers/SumOfNumbersKata.java
@@ -1,21 +1,13 @@
 package kyu7.sumofnumbers;
 
-import java.util.List;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class SumOfNumbersKata {
 
     public int getSum(int a, int b) {
         if (a == b) return a;
-        int lower, higher;
-        if (a >= b) {
-            higher = a;
-            lower = b;
-        } else {
-            higher = b;
-            lower = a;
-        }
+        int lower = Math.min(a,b);
+        int higher = Math.max(a,b);
 
         return IntStream.rangeClosed(lower,higher).sum();
     }
