f2f3e4483a0c451b7bd99cf4cacaeacfcd801e5d
juanMoleroD <juanldimeco@gmail.com>
2022-02-27 23:41:54 +0000

    Cleaned spacing and added variable to explain use of Math.sqrt() but it should be unnecessary. I think using the for loop explains the function of the code better than a while loop.

--- src/main/java/kyu5/productconsecfib/ProductConsecutiveFib.java
+++ src/main/java/kyu5/productconsecfib/ProductConsecutiveFib.java
@@ -3,25 +3,26 @@ package kyu5.productconsecfib;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.IntStream;
 
 public class ProductConsecutiveFib {
 
     public static long[] productFib(long prod) {
         List<Long> fibonacci = new ArrayList<>(Arrays.asList(1L,1L));
+        double loopLimit = Math.sqrt(prod);
 
-        for (int i = 1; i < Math.sqrt(prod); i++) {
+        for (int i = 1; i < loopLimit; i++) {
             fibonacci.add(fibonacci.get(i-1) + fibonacci.get(i));
 
             long current = fibonacci.get(i+1);
             long previous = fibonacci.get(i);
 
-            if (current * previous == prod) return new long[]{previous, current, 1};
-            if (current * previous > prod) return new long[]{previous, current, 0};
+            if (current * previous < prod) continue;
+
+            if (current * previous == prod) {
+                return new long[]{previous, current, 1};
+            } else return new long[]{previous, current, 0};
         }
 
         return new long[]{1, 1, 1};
-
     }
-
 }
