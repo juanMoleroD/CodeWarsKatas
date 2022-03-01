99fcdf16d201aed8f40cd170205a2f1bc389e098
juanMoleroD <juanldimeco@gmail.com>
2022-03-01 08:59:26 +0000

    Removed checked letters as not required.

--- src/main/java/kyu5/scramblies/ScrambliesKata.java
+++ src/main/java/kyu5/scramblies/ScrambliesKata.java
@@ -4,7 +4,6 @@ public class ScrambliesKata {
 
     public boolean scramble(String str1, String str2) {
         StringBuilder stringToCheck = new StringBuilder(str1);
-        StringBuilder letterFound = new StringBuilder();
 
         for (int i = 0; i < str2.length();i++) {
             String currentLetter = Character.toString(str2.charAt(i));
@@ -12,7 +11,6 @@ public class ScrambliesKata {
             if (stringToCheck.lastIndexOf(currentLetter) == -1) return false;
             int letterPositionOnStr = stringToCheck.lastIndexOf(currentLetter);
 
-            letterFound.append(stringToCheck.charAt(letterPositionOnStr));
             stringToCheck.setCharAt(letterPositionOnStr, '0');
         }
 
