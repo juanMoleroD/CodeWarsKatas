ad5db9620fe7f75b99aaacfdf2add9e974685b24
juanMoleroD <juanldimeco@gmail.com>
2022-03-01 08:19:38 +0000

    Passed first test with 1 letter. Need to implement more than one letter and repeated words.

--- src/main/java/kyu5/scramblies/ScrambliesKata.java
+++ src/main/java/kyu5/scramblies/ScrambliesKata.java
@@ -3,8 +3,15 @@ package kyu5.scramblies;
 public class ScrambliesKata {
 
     public boolean scramble(String str1, String str2) {
+        boolean strContainsLetter = false;
 
-        // your code
-        return false;
+        for (int i = 0; i < str2.length();i++) {
+            char currentLetter = str2.charAt(i);
+            for(int j = 0; j < str1.length(); j++) {
+                if (str1.charAt(j) == currentLetter) strContainsLetter = true;
+            }
+        }
+
+        return strContainsLetter;
     }
 }
