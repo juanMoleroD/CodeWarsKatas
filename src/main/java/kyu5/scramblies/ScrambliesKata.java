fdc2a08a49ef55384dabe4be0d4fba34e39d0520
juanMoleroD <juanldimeco@gmail.com>
2022-03-01 08:49:17 +0000

    Working logic so far, needs more testing and readability needs to be improved.

--- src/main/java/kyu5/scramblies/ScrambliesKata.java
+++ src/main/java/kyu5/scramblies/ScrambliesKata.java
@@ -4,14 +4,18 @@ public class ScrambliesKata {
 
     public boolean scramble(String str1, String str2) {
         boolean strContainsLetter = false;
+        StringBuilder stringToCheck = new StringBuilder(str1);
+        StringBuilder letterFound = new StringBuilder();
 
         for (int i = 0; i < str2.length();i++) {
-            char currentLetter = str2.charAt(i);
-            for(int j = 0; j < str1.length(); j++) {
-                if (str1.charAt(j) == currentLetter) strContainsLetter = true;
-            }
+            String currentLetter = Character.toString(str2.charAt(i));
+
+            if (stringToCheck.lastIndexOf(currentLetter) == -1) return false;
+
+            letterFound.append(stringToCheck.charAt(stringToCheck.lastIndexOf(currentLetter)));
+            stringToCheck.setCharAt(stringToCheck.lastIndexOf(currentLetter), '0');
         }
 
-        return strContainsLetter;
+        return true;
     }
 }
