0af3f413de68d59b27b46a10899a0809ecfff45e
juanMoleroD <juanldimeco@gmail.com>
2022-04-03 15:13:23 +0100

    All functionality added and tested

--- src/main/java/kyu4/rankingsystem/RankingSystemUser.java
+++ src/main/java/kyu4/rankingsystem/RankingSystemUser.java
@@ -1,21 +1,44 @@
 package kyu4.rankingsystem;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class RankingSystemUser {
 
     private int rank;
     private int progress;
+
+    final private List<Integer> listOfRanks = new ArrayList<>(List.of(-8,-7,-6,-5,-4,-3,-2,-1,1,2,3,4,5,6,7,8));
 
     public RankingSystemUser() {
         this.rank = -8;
         this.progress = 0;
     }
 
-    void incProgress(int testRank) {
-        progress = 10;
-        this.setRank(-7);
+    void incProgress(int activityRank) {
+        int currentRankIndex = listOfRanks.indexOf(getRank());
+        int activityRankIndex = listOfRanks.indexOf(activityRank);
+        int differenceInRankAndActivityIndex = activityRankIndex - currentRankIndex;
+
+        if (differenceInRankAndActivityIndex == 0) progress += 3;
+        if (differenceInRankAndActivityIndex == -1) progress += 1;
+        if (differenceInRankAndActivityIndex >= -2) progress += 0;
+
+        if (differenceInRankAndActivityIndex == 1) progress += 10;
+        if (differenceInRankAndActivityIndex == 2) progress += 40;
+        if (differenceInRankAndActivityIndex == 3) progress += 90;
+        if (differenceInRankAndActivityIndex >= 4) progress += 160;
+
+
+        if (getProgress() >= 100 ) levelUp();
+        if (listOfRanks.indexOf(getRank()) >= listOfRanks.size()-1) setProgress(0);
     }
 
-
+    private void levelUp() {
+        int currentRankIndex = listOfRanks.indexOf(getRank());
+        setRank(listOfRanks.get(currentRankIndex + 1));
+        setProgress(progress - 100);
+    }
 
 
 
@@ -24,7 +47,7 @@ public class RankingSystemUser {
     }
 
     void setRank(int rank) {
-        this.rank = rank;
+        if (listOfRanks.contains(rank)) this.rank = rank;
     }
 
     int getProgress() {
--- src/test/java/kyu4/rankingsystem/RankingSystemUserTest.java
+++ src/test/java/kyu4/rankingsystem/RankingSystemUserTest.java
@@ -16,7 +16,12 @@ class RankingSystemUserTest {
 
     @Test
     void user_startsAtRank8() {
+        assertEquals(-8, user.getRank());
+    }
 
+    @Test
+    void userRankIsOnlyFromAllowedList() {
+        user.setRank(-9);
         assertEquals(-8, user.getRank());
     }
 
@@ -28,10 +33,60 @@ class RankingSystemUserTest {
 
     @Test
     void user_canIncreaseProgress() {
-        int testRank = -7;
-        user.incProgress(testRank);
+        user.incProgress(-7);
         assertEquals(10, user.getProgress());
     }
+
+    @Test
+    void ifActivityRank_sameAsRank_increaseProgressBy3() {
+        user.incProgress(-8);
+        assertEquals(3, user.getProgress());
+    }
+
+    @Test
+    void ifActivityRank_1RanksBelow_increaseProgressBy1() {
+        user.setRank(-6);
+        user.incProgress(-7);
+        assertEquals(1, user.getProgress());
+    }
+
+    @Test
+    void ifActivityRank_2RanksBelow_ignore() {
+        user.setRank(-6);
+        user.incProgress(-8);
+        assertEquals(0, user.getProgress());
+    }
+
+    @Test
+    void ifActivityRank_1RankAbove_increaseProgressBy10() {
+        user.incProgress(-7);
+        assertEquals(10, user.getProgress());
+    }
+
+    @Test
+    void ifActivityRank_2RanksAbove_increaseProgressBy40() {
+        user.incProgress(-6);
+        assertEquals(40, user.getProgress());
+    }
+
+    @Test
+    void ifActivityRank_3RanksAbove_increaseProgressBy90() {
+        user.incProgress(-5);
+        assertEquals(90, user.getProgress());
+    }
+
+    @Test
+    void ifActivityRank_4RanksAbove_increaseProgressBy160() {
+        user.incProgress(-4);
+        assertEquals(60, user.getProgress());
+    }
+
+    @Test
+    void ifActivityRank_over4RanksAbove_increaseProgressBy160() {
+        user.incProgress(-3);
+        assertEquals(60, user.getProgress());
+    }
+
 
     @Test
     void user_levelsUp_ifProgressReaches100() {
@@ -41,10 +96,55 @@ class RankingSystemUserTest {
     }
 
     @Test
+    void userOnlyLevelsUp_whenProgressOver99() {
+        user.incProgress(-7);
+        assertEquals(-8, user.getRank());
+    }
+
+    @Test
     void when_user_levelsUp_Progress_goesBack100(){
         user.setProgress(90);
         user.incProgress(-7);
-        assertEquals(0, user.getRank());
+        assertEquals(0, user.getProgress());
+    }
+
+    @Test
+    void when_user_levelsUp_Progress_goesBack100_andSavesExtra(){
+        user.setProgress(99);
+        user.incProgress(-7);
+        assertEquals(9, user.getProgress());
+    }
+
+    @Test
+    void levelUpAfterMinus7_isMinus6(){
+        user.setRank(-7);
+        user.setProgress(95);
+        user.incProgress(-6);
+        assertEquals(-6, user.getRank());
+    }
+
+    @Test
+    void levelUpAfterMinus1_is1() {
+        user.setRank(-1);
+        user.setProgress(95);
+        user.incProgress(1);
+        assertEquals(1, user.getRank());
+    }
+
+    @Test
+    void userProgressMaxIs0_whenLevelingUpTo8() {
+        user.setRank(7);
+        user.setProgress(95);
+        user.incProgress(8);
+        assertEquals(0, user.getProgress());
+    }
+
+    @Test
+    void userProgressMaxIs0_whenAlready_Level8() {
+        user.setRank(8);
+        user.setProgress(5);
+        user.incProgress(8);
+        assertEquals(0, user.getProgress());
     }
 
 
