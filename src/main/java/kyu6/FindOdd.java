e0aa27f8564e69c8b0735fc9f1ec607bb4451bb8
juanMoleroD <juanldimeco@gmail.com>
2022-09-16 11:26:13 +0100

    Completed kata

---
+++ src/main/java/kyu6/FindOdd.java
@@ -0,0 +1,22 @@
+package kyu6;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class FindOdd {
+    public static int findIt(int[] a) {
+        Map<Integer, Integer> dict = new HashMap<>();
+        Arrays.stream(a).forEach(element -> {
+            if (dict.containsKey(element)) {
+                dict.replace(element, dict.get(element) + 1);
+            } else {
+                dict.put(element, 1);
+            }
+        });
+        Map<Integer, Integer> result = dict.entrySet().stream()
+                                        .filter(entry -> entry.getValue() % 2 != 0)
+                                        .collect(Collectors.toMap(entry -> entry.getKey(), entry -> entry.getValue()));
+
+        return Integer.parseInt(result.keySet().toArray()[0].toString());
+    }
+}
