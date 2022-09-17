96f17b28cac9e0a0f85127284043bd50bb24b96b
juanMoleroD <juanldimeco@gmail.com>
2022-09-17 12:07:36 +0100

    added new kata for inverted values

---
+++ src/main/java/kyu8/invertvalues/InvertValues.java
@@ -0,0 +1,7 @@
+package kyu8.invertvalues;
+
+public class InvertValues {
+    public static int[] invert(int[] array) {
+        return null;
+    }
+}
---
+++ src/test/java/kyu8/invertvalues/InvertValuesTest.java
@@ -0,0 +1,12 @@
+package kyu8.invertvalues;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InvertValuesTest {
+
+    @Test
+    void invert() {
+    }
+}
