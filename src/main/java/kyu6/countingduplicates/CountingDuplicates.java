1f2d87b9240e4bd711c937f072125fd4cd9adc73
juanMoleroD <juanldimeco@gmail.com>
2022-09-17 18:28:28 +0100

    added new kata

---
+++ src/main/java/kyu6/countingduplicates/CountingDuplicates.java
@@ -0,0 +1,8 @@
+package kyu6.countingduplicates;
+
+public class CountingDuplicates {
+    public static int duplicateCount(String text) {
+        // Write your code here
+        return -1;
+    }
+}
