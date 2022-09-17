c99b3cd44f6a79b86b405a968e36c3a2b88c7b52
juanMoleroD <juanldimeco@gmail.com>
2022-09-17 12:35:10 +0100

    refactor

--- src/main/java/kyu7/oddoreven/OddOrEven.java
+++ src/main/java/kyu7/oddoreven/OddOrEven.java
@@ -1,11 +1,8 @@
 package kyu7.oddoreven;
 
-import java.util.Arrays;
-
 public class OddOrEven {
     public static String oddOrEven (int[] array) {
-        // your code
-        int sum = Arrays.stream(array).sum();
+        int sum = java.util.Arrays.stream(array).sum();
         return sum % 2 == 0 ? "even": "odd" ;
     }
 }
