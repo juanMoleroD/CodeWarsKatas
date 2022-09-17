a2223386b855da13445e2cfdba40442f84d06052
juanMoleroD <juanldimeco@gmail.com>
2022-09-17 18:10:51 +0100

    added solution

--- src/main/java/kyu6/validwalk/ValidWalk.java
+++ src/main/java/kyu6/validwalk/ValidWalk.java
@@ -3,6 +3,16 @@ package kyu6.validwalk;
 public class ValidWalk {
     public static boolean isValid(char[] walk) {
         // Insert brilliant code here
+        if (walk.length == 10) {
+            int x = 0, y = 0;
+            for( char direction : walk ) {
+                if (direction == 'n') y += 1;
+                if (direction == 's') y -= 1;
+                if (direction == 'e') x += 1;
+                if (direction == 'w') x -= 1;
+            }
+            return x == 0 && y == 0;
+        }
         return false;
     }
 }
--- src/test/java/kyu6/validwalk/ValidWalkTest.java
+++ src/test/java/kyu6/validwalk/ValidWalkTest.java
@@ -11,6 +11,7 @@ class ValidWalkTest {
     @Test
     public void Test() {
         assertEquals(true, TenMinWalk.isValid(new char[] {'n','s','n','s','n','s','n','s','n','s'}), "Should return true");
+        assertEquals(true, TenMinWalk.isValid(new char[] {'n','s','e','w','n','s','w','e','e','w'}), "Should return true");
         assertEquals( false, TenMinWalk.isValid(new char[] {'w','e','w','e','w','e','w','e','w','e','w','e'}), "Should return false");
         assertEquals( false, TenMinWalk.isValid(new char[] {'w'}), "Should return false");
         assertEquals( false, TenMinWalk.isValid(new char[] {'n','n','n','s','n','s','n','s','n','s'}), "Should return false");
