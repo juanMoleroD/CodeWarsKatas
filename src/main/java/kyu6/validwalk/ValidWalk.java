e64f5178ae38fea5c8837c0678f2178ab5837226
juanMoleroD <juanldimeco@gmail.com>
2022-09-17 12:53:56 +0100

    Added new Kata to resolve

---
+++ src/main/java/kyu6/validwalk/ValidWalk.java
@@ -0,0 +1,8 @@
+package kyu6.validwalk;
+
+public class ValidWalk {
+    public static boolean isValid(char[] walk) {
+        // Insert brilliant code here
+        return false;
+    }
+}
---
+++ src/test/java/kyu6/validwalk/ValidWalkTest.java
@@ -0,0 +1,18 @@
+package kyu6.validwalk;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ValidWalkTest {
+
+    ValidWalk TenMinWalk = new ValidWalk();
+
+    @Test
+    public void Test() {
+        assertEquals(true, TenMinWalk.isValid(new char[] {'n','s','n','s','n','s','n','s','n','s'}), "Should return true");
+        assertEquals( false, TenMinWalk.isValid(new char[] {'w','e','w','e','w','e','w','e','w','e','w','e'}), "Should return false");
+        assertEquals( false, TenMinWalk.isValid(new char[] {'w'}), "Should return false");
+        assertEquals( false, TenMinWalk.isValid(new char[] {'n','n','n','s','n','s','n','s','n','s'}), "Should return false");
+    }
+}
