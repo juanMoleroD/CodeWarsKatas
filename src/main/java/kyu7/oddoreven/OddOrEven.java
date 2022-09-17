c2ca57ffadae53579f227882258b0e83abf36f04
juanMoleroD <juanldimeco@gmail.com>
2022-09-17 12:34:38 +0100

    completed solution

--- src/main/java/kyu7/oddoreven/OddOrEven.java
+++ src/main/java/kyu7/oddoreven/OddOrEven.java
@@ -1,9 +1,11 @@
 package kyu7.oddoreven;
 
+import java.util.Arrays;
+
 public class OddOrEven {
     public static String oddOrEven (int[] array) {
         // your code
-
-        return "-1";
+        int sum = Arrays.stream(array).sum();
+        return sum % 2 == 0 ? "even": "odd" ;
     }
 }
