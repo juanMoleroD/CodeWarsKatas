6c2b6e553ef5e622a7a06acb336c8bd693d28c0f
juanMoleroD <juanldimeco@gmail.com>
2022-11-08 14:47:53 +0000

    Added new Kata

---
+++ src/main/java/kyu3/makeaspiral/Spiralizor.java
@@ -0,0 +1,7 @@
+package kyu3.makeaspiral;
+
+public class Spiralizor {
+    public static int[][] spiralize(int size) {
+        return null;
+    }
+}
---
+++ src/test/java/kyu3/makeaspiral/SpiralizorTest.java
@@ -0,0 +1,37 @@
+package kyu3.makeaspiral;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SpiralizorTest {
+
+    @Test
+    public void test5() {
+        assertArrayEquals(
+                new int[][] {
+                        { 1, 1, 1, 1, 1 },
+                        { 0, 0, 0, 0, 1 },
+                        { 1, 1, 1, 0, 1 },
+                        { 1, 0, 0, 0, 1 },
+                        { 1, 1, 1, 1, 1 }
+                },
+                Spiralizor.spiralize(5));
+    }
+
+    @Test
+    public void test8() {
+        assertArrayEquals(
+                new int[][] {
+                        { 1, 1, 1, 1, 1, 1, 1, 1 },
+                        { 0, 0, 0, 0, 0, 0, 0, 1 },
+                        { 1, 1, 1, 1, 1, 1, 0, 1 },
+                        { 1, 0, 0, 0, 0, 1, 0, 1 },
+                        { 1, 0, 1, 0, 0, 1, 0, 1 },
+                        { 1, 0, 1, 1, 1, 1, 0, 1 },
+                        { 1, 0, 0, 0, 0, 0, 0, 1 },
+                        { 1, 1, 1, 1, 1, 1, 1, 1 },
+                },
+                Spiralizor.spiralize(8));
+    }
+}
