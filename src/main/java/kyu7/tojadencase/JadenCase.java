6d29a2392df8f8e4f35d7ecef640276737dfcd08
juanMoleroD <juanldimeco@gmail.com>
2022-11-09 14:50:19 +0000

    cleaned solution

--- src/main/java/kyu7/tojadencase/JadenCase.java
+++ src/main/java/kyu7/tojadencase/JadenCase.java
@@ -1,15 +1,12 @@
 package kyu7.tojadencase;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
-import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class JadenCase {
         public String toJadenCase(String phrase) {
 //            put your code below this comment
-            if (phrase == null || Objects.equals(phrase, "")) return null;
+            if (phrase == null || phrase.equals("")) return null;
             return Arrays.stream(phrase.split(" "))
                     .map(word -> word.substring(0,1).toUpperCase() + word.substring(1))
                     .collect(Collectors.joining(" "));
