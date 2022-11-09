07d01984c911ec525865ac51444a8832f7cabf32
juanMoleroD <juanldimeco@gmail.com>
2022-11-09 14:42:13 +0000

    Completed Kata

--- src/main/java/kyu7/tojadencase/JadenCase.java
+++ src/main/java/kyu7/tojadencase/JadenCase.java
@@ -1,11 +1,18 @@
 package kyu7.tojadencase;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
 public class JadenCase {
         public String toJadenCase(String phrase) {
 //            put your code below this comment
-
-
-            return null;
+            if (phrase == null || Objects.equals(phrase, "")) return null;
+            return Arrays.stream(phrase.split(" "))
+                    .map(word -> word.substring(0,1).toUpperCase() + word.substring(1))
+                    .collect(Collectors.joining(" "));
         }
 
 }
--- src/test/java/kyu7/tojadencase/JadenCaseTest.java
+++ src/test/java/kyu7/tojadencase/JadenCaseTest.java
@@ -10,18 +10,16 @@ class JadenCaseTest {
 
     @Test
     public void test() {
-        assertEquals("toJadenCase doesn't return a valid JadenCase String! try again please :)",
-                "Most Trees Are Blue",
-                jadenCase.toJadenCase("most trees are blue"));
+        assertEquals("Most Trees Are Blue", jadenCase.toJadenCase("most trees are blue"), "toJadenCase doesn't return a valid JadenCase String! try again please :)");
     }
 
     @Test
     public void testNullArg() {
-        assertNull("Must return null when the arg is null", jadenCase.toJadenCase(null));
+        assertNull(jadenCase.toJadenCase(null), "Must return null when the arg is null");
     }
 
     @Test
     public void testEmptyArg() {
-        assertNull("Must return null when the arg is empty string", jadenCase.toJadenCase(""));
+        assertNull(jadenCase.toJadenCase(""), "Must return null when the arg is empty string");
     }
 }
