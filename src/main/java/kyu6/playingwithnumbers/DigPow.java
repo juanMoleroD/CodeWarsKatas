649ceb76cfac92fc6f1c045f878bbcf7598b44ff
juanMoleroD <juanldimeco@gmail.com>
2022-11-10 09:55:03 +0000

    Added new Kata

---
+++ src/main/java/kyu6/playingwithnumbers/DigPow.java
@@ -0,0 +1,13 @@
+package kyu6.playingwithnumbers;
+
+public class DigPow {
+    public static long digPow(int n, int p) {
+        // your code
+        return -1;
+    }
+
+    static long getPoweredSum(int n, int p) {
+
+    }
+
+}
---
+++ src/test/java/kyu6/playingwithnumbers/DigPowTest.java
@@ -0,0 +1,27 @@
+package kyu6.playingwithnumbers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DigPowTest {
+
+    @Test
+    void canReturnDigitPower() {
+        assertEquals(5, DigPow.getPoweredSum(12,1));
+    }
+
+    //    @Test
+//    public void Test1() {
+//        assertEquals(1, DigPow.digPow(89, 1));
+//    }
+//    @Test
+//    public void Test2() {
+//        assertEquals(-1, DigPow.digPow(92, 1));
+//    }
+//    @Test
+//    public void Test3() {
+//        assertEquals(51, DigPow.digPow(46288, 3));
+//    }
+
+}
