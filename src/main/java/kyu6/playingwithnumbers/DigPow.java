ec4f630344a20414ac8f5a41a4d48399a55bd5d2
juanMoleroD <juanldimeco@gmail.com>
2022-11-10 10:16:24 +0000

    Completed kata

--- src/main/java/kyu6/playingwithnumbers/DigPow.java
+++ src/main/java/kyu6/playingwithnumbers/DigPow.java
@@ -3,19 +3,30 @@ package kyu6.playingwithnumbers;
 import java.util.Arrays;
 
 public class DigPow {
+
     public static long digPow(int n, int p) {
         // your code
-        return -1;
+        return checkForPotentialMultiple(n, getPoweredSum(n, p));
     }
 
     static long getPoweredSum(int n, int p) {
         String num = String.valueOf(n);
         String[] nums = num.split("");
-        System.out.println(Arrays.toString(nums));
         long result = 0;
         for(int i = 0; i< nums.length; i++) {
             result += Math.pow(Integer.parseInt(nums[i]), i + p);
         }
+        return result;
+    }
+
+    static long checkForPotentialMultiple(int n, long powSum) {
+        long result = -1;
+
+        for (int i = 1; i * n <= powSum; i++) {
+            long current = i * n;
+            if ( current == powSum) result = i;
+        }
+
         return result;
     }
 
--- src/test/java/kyu6/playingwithnumbers/DigPowTest.java
+++ src/test/java/kyu6/playingwithnumbers/DigPowTest.java
@@ -12,17 +12,17 @@ class DigPowTest {
         assertEquals(31, DigPow.getPoweredSum(23,2));
     }
 
-    //    @Test
-//    public void Test1() {
-//        assertEquals(1, DigPow.digPow(89, 1));
-//    }
-//    @Test
-//    public void Test2() {
-//        assertEquals(-1, DigPow.digPow(92, 1));
-//    }
-//    @Test
-//    public void Test3() {
-//        assertEquals(51, DigPow.digPow(46288, 3));
-//    }
+        @Test
+    public void Test1() {
+        assertEquals(1, DigPow.digPow(89, 1));
+    }
+    @Test
+    public void Test2() {
+        assertEquals(-1, DigPow.digPow(92, 1));
+    }
+    @Test
+    public void Test3() {
+        assertEquals(51, DigPow.digPow(46288, 3));
+    }
 
 }
