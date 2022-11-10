8a0ba8109ed08f28acbe92f1d7f33967761015fe
juanMoleroD <juanldimeco@gmail.com>
2022-11-10 10:19:12 +0000

    Refactored

--- src/main/java/kyu6/playingwithnumbers/DigPow.java
+++ src/main/java/kyu6/playingwithnumbers/DigPow.java
@@ -10,23 +10,15 @@ public class DigPow {
     }
 
     static long getPoweredSum(int n, int p) {
-        String num = String.valueOf(n);
-        String[] nums = num.split("");
+        String[] nums = String.valueOf(n).split("");
         long result = 0;
-        for(int i = 0; i< nums.length; i++) {
-            result += Math.pow(Integer.parseInt(nums[i]), i + p);
-        }
+        for (int i = 0; i< nums.length; i++) result += Math.pow(Integer.parseInt(nums[i]), i + p);
         return result;
     }
 
     static long checkForPotentialMultiple(int n, long powSum) {
         long result = -1;
-
-        for (int i = 1; i * n <= powSum; i++) {
-            long current = i * n;
-            if ( current == powSum) result = i;
-        }
-
+        for (long i = 1; i * n <= powSum; i++) if ( i * n == powSum) result = i;
         return result;
     }
 
